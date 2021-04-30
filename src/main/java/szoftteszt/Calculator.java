8ea0cd371156d66b8395acb8f35ccadcc14b84da
David <szuhid@gmail.com>
2021-04-30 17:24:22 +0200

    tests added

--- src/main/java/szoftteszt/Calculator.java
+++ src/main/java/szoftteszt/Calculator.java
@@ -2,19 +2,27 @@ package szoftteszt;
 
 public class Calculator {
 
-    public int Average(int[] numbers){
+    public double Average(int[] numbers){
         int price = 0;
-        int avg;
-        for(int i = 0; i< numbers.length; i++){
-            price += numbers[i];
+        double avg;
+        for (int number : numbers) {
+            price += number;
         }
-        avg = price/numbers.length;
-        return avg;
+        /*for(int i = 0; i< numbers.length; i++){
+            price += numbers[i];
+        }*/
+        avg = (double)price/numbers.length;
+        double scale = Math.pow(10, 2);
+        return Math.round(avg * scale) / scale;
     }
 
-    public double CalcPrice(int landCost, int sell, int yield){
+    public double ProjPrice(int landCost, int sell, int yield){
         double prediction =  (double)landCost/(double)(yield*sell);
         double scale = Math.pow(10, 2);
         return Math.round(prediction * scale) / scale;
     }
+
+
+
+
 }

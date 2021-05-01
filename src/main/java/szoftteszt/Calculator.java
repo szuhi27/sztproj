b87b77355ac5906dc5fdae6e0ae00d117e48c1f5
David <szuhid@gmail.com>
2021-05-01 11:47:37 +0200

    File not-comply fix?!

--- src/main/java/szoftteszt/Calculator.java
+++ src/main/java/szoftteszt/Calculator.java
@@ -2,27 +2,23 @@ package szoftteszt;
 
 public class Calculator {
 
-    public double Average(int[] numbers){
-        int price = 0;
-        double avg;
-        for (int number : numbers) {
-            price += number;
-        }
-        /*for(int i = 0; i< numbers.length; i++){
-            price += numbers[i];
-        }*/
-        avg = (double)price/numbers.length;
-        double scale = Math.pow(10, 2);
-        return Math.round(avg * scale) / scale;
+  public double Average(int[] numbers) {
+    int price = 0;
+    double avg;
+    for (int number : numbers) {
+      price += number;
     }
+    /*for(int i = 0; i< numbers.length; i++){
+        price += numbers[i];
+    }*/
+    avg = (double) price / numbers.length;
+    double scale = Math.pow(10, 2);
+    return Math.round(avg * scale) / scale;
+  }
 
-    public double ProjPrice(int landCost, int sell, int yield){
-        double prediction =  (double)landCost/(double)(yield*sell);
-        double scale = Math.pow(10, 2);
-        return Math.round(prediction * scale) / scale;
-    }
-
-
-
-
+  public double ProjPrice(int landCost, int sell, int yield) {
+    double prediction = (double) landCost / (double) (yield * sell);
+    double scale = Math.pow(10, 2);
+    return Math.round(prediction * scale) / scale;
+  }
 }
--- src/test/java/szoftteszt/CalculatorTest.java
+++ src/test/java/szoftteszt/CalculatorTest.java
@@ -9,37 +9,36 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
-    private Calculator calculator;
+  private Calculator calculator;
 
-    @BeforeEach
-    public void setUp() {
-        calculator = new Calculator();
-    }
+  @BeforeEach
+  public void setUp() {
+    calculator = new Calculator();
+  }
 
-    @Test
-    @DisplayName("Average should work")
-    public void testAverage(){
-        int[] array = {5,5,5,5};
-        assertEquals(5.00, calculator.Average(array), "Average should work");
-    }
+  @Test
+  @DisplayName("Average should work")
+  public void testAverage() {
+    int[] array = {5, 5, 5, 5};
+    assertEquals(5.00, calculator.Average(array), "Average should work");
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Average repeat should work")
-    public void testAverageRepeat(){
-        int[] array = {5,3,4,6,8,9,1,6,20,4,61,32};
-        assertEquals(13.25, calculator.Average(array), "Average repeat should work");
-    }
+  @RepeatedTest(5)
+  @DisplayName("Average repeat should work")
+  public void testAverageRepeat() {
+    int[] array = {5, 3, 4, 6, 8, 9, 1, 6, 20, 4, 61, 32};
+    assertEquals(13.25, calculator.Average(array), "Average repeat should work");
+  }
 
-    @Test
-    @DisplayName("Projection should work")
-    public void testProjected(){
-        assertEquals(2.08, calculator.ProjPrice(1250,5,120),"Projection should work");
-    }
+  @Test
+  @DisplayName("Projection should work")
+  public void testProjected() {
+    assertEquals(2.08, calculator.ProjPrice(1250, 5, 120), "Projection should work");
+  }
 
-    @RepeatedTest(3)
-    @DisplayName("Projection repeat should work")
-    public void testProjectedRepeat(){
-        assertEquals(8.01, calculator.ProjPrice(23000,8,359),"Projection repeat should work");
-    }
-
+  @RepeatedTest(3)
+  @DisplayName("Projection repeat should work")
+  public void testProjectedRepeat() {
+    assertEquals(8.01, calculator.ProjPrice(23000, 8, 359), "Projection repeat should work");
+  }
 }
